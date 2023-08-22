3b1d4e06b6c3204c86433c1d8f9766a394e8f15c
Adrian Folge <adrian.folge@hotmail.com>
2023-08-22 14:35:22 +0200

    feat: lenge siden

---
+++ naturninjasanity/schemas/profile.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'profile',
+    title: 'Profile',
+    type: 'document',
+    fields: [
+        {
+            name: 'profile_img_path',
+            title: 'Profile image path',
+            type: 'string',
+
+        },
+        {
+            name: 'banner_image_path',
+            title: 'Banner image path',
+            type: 'string', 
+        },
+        {
+            name: 'mail',
+            title: 'Mail',
+            type: 'string', 
+        },
+    ]
+}
